63c19b98f53fedaef573d02068fd2b8c4f354725
Utkarsh Patel <itismeutkarsh@gmail.com>
2023-09-16 13:54:42 +0530

    Init

---
+++ README.md
@@ -0,0 +1,44 @@
+# Alfred Workflow: Upload Screenshot to Cloudflare R2 Storage
+
+This workflow helps you upload a screenshot from your clipboard or local disk to Cloudflare R2 and puts the public URL of the image to your clipboard.
+
+## Overview
+
+This workflow is written in Python. It uses Boto3 as the AWS client to upload images.
+
+## Download
+
+https://github.com/PatelUtkarsh/Alfred-Workflow-Upload-R2/releases
+
+## Usage
+
+Check https://www.cloudflare.com/developer-platform/r2/ on how to get environment variables.
+
+Config Environment Variables:
+
+- cf_access_key: Cloudflare access key
+- cf_account_id: Cloudflare access secret
+- cf_secret_key: Cloudflare access secret
+- cf_bucket_name: Cloudflare bucket name. e.g. `my-bucket-name`
+- tinypng_api_key: Get your tinypng API key from https://tinypng.com/developers to compress images before uploading.
+- shorturl: If you have proxy to bucket via CloudFlare use that url.
+
+Upload image from clipboard:
+
+```bash
+upload
+```
+
+Upload image from local:
+
+```bash
+upload TYPE-FILENAME-HERE
+```
+
+Upload clipboard text file:
+```bash
+pb
+```
+
+## Forked source: 🙌
+- https://github.com/tonyxu-io/Alfred-Workflow-Upload-S3
---
+++ upload-s3.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import datetime
+import os
+import sys
+import atexit
+import random
+import string
+import mimetypes
+import subprocess
+import tempfile
+from subprocess import call
+from os.path import expanduser, exists, basename, getsize
+from workflow import Workflow
+
+def get_random_bit(length):
+    return ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(length))
+
+def capture_text():
+    # global name 'subprocess' is not defined
+    text_file = subprocess.check_output("pbpaste", shell=True)
+    random_bit = get_random_bit(10)
+    file_name = 'clip-'+random_bit+'.txt'
+    file_path = os.path.join(tempfile.mkdtemp(), file_name)
+    with open(file_path, "wb") as f:
+        f.write(text_file)
+    return file_path, file_name, 'text/plain'
+
+def capture():
+    random_bit = get_random_bit(5)
+    file_name = datetime.datetime.now().strftime('%S'+random_bit+'.png')
+    content_type = 'image/png'
+    if (sys.argv[1] != ""):
+        file_path = sys.argv[1]
+        content_type = mimetypes.MimeTypes().guess_type(file_path)[0]
+        _head, file_name = os.path.split(file_path)
+        file_names = file_name.split('.')
+        file_names.insert(1,'-' + random_bit + '.')
+        file_name = "".join(file_names)
+    else:
+        file_path = os.path.join(tempfile.mkdtemp(), file_name)
+        atexit.register(lambda x: os.remove(x) if os.path.exists(x) else None, file_path)
+        save = call(['./pngpaste', file_path])
+        if save == 1:
+            sys.exit()
+    tinypng_key = os.getenv('tinypng_api_key')
+    # If content type starts with 'image/' then compress.
+    if content_type.startswith('image/') and tinypng_key:
+        import tinify
+        tinify.key = tinypng_key
+        tinify.from_file(file_path).to_file(file_path)
+    return file_path, file_name, content_type
+
+def main(wf):
+    import boto3
+    # if cmd is pressed we assume it is text capture.
+    if sys.argv[1] == "text":
+        file_path, file_name, content_type = capture_text()
+    else:
+        file_path, file_name, content_type = capture()
+    account_id = os.getenv('cf_account_id')
+    bucket_name = os.getenv('cf_bucket_name')
+    s3 = boto3.client(
+        service_name='s3',
+        aws_access_key_id=os.getenv('cf_access_key'),
+        aws_secret_access_key=os.getenv('cf_secret_key'),
+        endpoint_url="https://%s.r2.cloudflarestorage.com/%s" %(account_id, bucket_name)
+    )
+    s3.upload_file(file_path, bucket_name, file_name, ExtraArgs={'ContentType': content_type})
+    shorturl = os.getenv('shorturl')
+    # if short url is not empty and exists.
+    if shorturl:
+        if shorturl[len(shorturl)-1] != "/":
+            shorturl = shorturl + "/"
+        output = "%s%s" %(shorturl,file_name)
+    else:
+        output = 'https://pub-%s.r2.dev/%s/%s' %(account_id,bucket_name,file_name)
+    print (output,end='')
+
+if __name__ == '__main__':
+    wf = Workflow(libraries=['./lib'])
+    sys.exit(wf.run(main))
